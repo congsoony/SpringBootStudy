c64d914aa5f39601f44cccf1e30456d92ab8a503
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2020-12-25 22:52:41 +0900

    @Getmmaping exLink 추가

--- ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
+++ ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.ex3.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.tomcat.jni.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +24,7 @@ public class SampleController {
         log.info("ex1........");
     }
 
-    @GetMapping({"/ex2"})
+    @GetMapping({"/ex2","/exLink"})
     public void exModel(Model model){
         List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream()
                 .mapToObj(i ->{
