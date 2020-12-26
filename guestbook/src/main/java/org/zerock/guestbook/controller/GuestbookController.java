8b09bb1f2cc72048eb78f7d70881d01f5b3d7357
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2020-12-26 11:56:17 +0900

    requestmapping 추가

--- guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/guestbook")
 @Log4j2
 public class GuestbookController {
 
