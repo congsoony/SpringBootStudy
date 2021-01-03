1d121217f6e1ef90f242fd22d81a4d770e22e3f9
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-03 15:22:32 +0900

    BackEnd : 등록 처리 Test 추가

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,15 +1,30 @@
 package org.zerock.guestbook.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor //의존성 자동주입
 public class GuestbookServiceImpl implements GuestbookService {
+
+	private final GuestbookRepository repository;
+
 	@Override
 	public Long register(GuestbookDTO dto) {
-		return null;
+		log.info("DTO---------------");
+		log.info(dto);
+
+		Guestbook entity = dtoToEntity(dto);
+		log.info(entity);
+
+		repository.save(entity);
+
+		return entity.getGno();
 	}
 }
