249cd29402557885a76307cc468a529c4061b56e
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-04 15:47:44 +0900

    BackEnd : application 클래스 @EnableJpaAuditing 추가

--- board/src/main/java/org/zerock/board/BoardApplication.java
+++ board/src/main/java/org/zerock/board/BoardApplication.java
@@ -2,8 +2,10 @@ package org.zerock.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing//자동으로 시간처리를 위한 어노테이션
 public class BoardApplication {
 
 	public static void main(String[] args) {
