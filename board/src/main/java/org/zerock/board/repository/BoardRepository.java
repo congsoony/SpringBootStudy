d4f899e11ac7816b8720e046c898c5b579a65cc1
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-04 17:43:24 +0900

    BackEnd : Board ,Member 100개 데이터 추가 ,Reply 댓글 board 번호의 임의로 300개 데이터 추가

--- board/src/main/java/org/zerock/board/repository/BoardRepository.java
+++ board/src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.board.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
+
 }
--- board/src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ board/src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -3,5 +3,7 @@ package org.zerock.board.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.board.entity.Reply;
 
+
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 }
