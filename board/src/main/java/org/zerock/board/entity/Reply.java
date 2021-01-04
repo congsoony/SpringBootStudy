c0658ddc642edbb73cc0cf06bf539816d5ef84ca
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-04 15:57:21 +0900

    BackEnd : Reply생성, BaseEntity상속, 컬럼 :rno ->pk, text, replyer

--- board/src/main/java/org/zerock/board/entity/Reply.java
+++ board/src/main/java/org/zerock/board/entity/Reply.java
@@ -1,2 +1,25 @@
-package org.zerock.board.entity;public class Reply {
+package org.zerock.board.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Reply extends BaseEntity{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long rno;
+
+	private String text;
+
+	private String replyer;
+	
 }
