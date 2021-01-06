a3c02d03f81b18a7f408f2622b2ab2d407dbdeec
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-06 18:18:12 +0900

    BackEnd: 게시물 목록, 게시물 조회 추가, Test완료

--- board/src/test/java/org/zerock/board/service/BoardServiceTests.java
+++ board/src/test/java/org/zerock/board/service/BoardServiceTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.dto.PageResultDTO;
 
 @SpringBootTest
 public class BoardServiceTests {
 	@Autowired
 	private BoardService boardService;
-	
+
 	@Test
 	public void testRegister(){
 		BoardDTO dto = BoardDTO.builder()
@@ -18,5 +20,23 @@ public class BoardServiceTests {
 				.writerEmail("user55@aaa.com")
 				.build();
 		Long bno = boardService.register(dto);
+	}
+
+	@Test
+	public void testList(){
+		PageRequestDTO pageRequestDTO = new PageRequestDTO();
+		PageResultDTO<BoardDTO, Object[]> result =boardService.getList(pageRequestDTO);
+		for(BoardDTO boardDTO : result.getDtoList()){
+			System.out.println(boardDTO);
+		}
+	}
+
+	@Test
+	public void testGet(){
+		Long bno = 100L;
+
+		BoardDTO boardDTO = boardService.get(bno);
+
+		System.out.println(boardDTO);
 	}
 }
