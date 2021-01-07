6f74b5e0408687cc37f6a36df8c8cd2cffe8e32b
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-07 17:33:48 +0900

    BackEnd : JPQLQuery select 테스트

---
+++ board/src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -0,0 +1,36 @@
+package org.zerock.board.repository.search;
+
+import com.querydsl.jpa.JPQLQuery;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.QBoard;
+
+import java.util.List;
+
+@Log4j2
+public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository{
+
+	public SearchBoardRepositoryImpl() {
+		super(Board.class);
+	}
+
+	@Override
+	public Board search1() {
+
+		log.info("search1....................");
+
+		QBoard board =QBoard.board;
+
+		JPQLQuery<Board> jpqlQuery =from(board);
+		jpqlQuery.select(board).where(board.bno.eq(1L));
+
+		log.info("--------------------");
+		log.info(jpqlQuery);
+		log.info("-----------------");
+
+		List<Board> result = jpqlQuery.fetch();
+
+		return null;
+	}
+}
