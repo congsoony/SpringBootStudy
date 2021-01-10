8413ea09701916b7645b3c6240d1e3c62597cd55
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-10 16:49:14 +0900

    BackEnd : Movie, MovieImage 레파지토리 생성

---
+++ mreivew/src/main/java/com/example/mreivew/repository/MovieImageRepository.java
@@ -0,0 +1,7 @@
+package com.example.mreivew.repository;
+
+import com.example.mreivew.entity.MovieImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieImageRepository extends JpaRepository<MovieImage,Long> {
+}
