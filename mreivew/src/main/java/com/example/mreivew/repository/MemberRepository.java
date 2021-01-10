5e37aa4f9437d8d963ecf59b520cb1261e7fd98d
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-10 21:51:02 +0900

    BackEnd:  MemberRepsotory 생성 Member 100개 생성

---
+++ mreivew/src/main/java/com/example/mreivew/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.example.mreivew.repository;
+
+import com.example.mreivew.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
