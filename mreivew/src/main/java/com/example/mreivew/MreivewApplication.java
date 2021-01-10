14ffcfac0ab74a3d0cc4f3c0164ffb2bd282f203
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-10 15:52:07 +0900

    spring boot 프로젝트 생성 api : Spring Boot DevTools , Lombok, Spring Web, Thymeleaf , Spring Data JPA 추가

---
+++ mreivew/src/main/java/com/example/mreivew/MreivewApplication.java
@@ -0,0 +1,13 @@
+package com.example.mreivew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MreivewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MreivewApplication.class, args);
+	}
+
+}
---
+++ mreivew/src/test/java/com/example/mreivew/MreivewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mreivew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MreivewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
