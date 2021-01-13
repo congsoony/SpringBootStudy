faa6d5d2da73759d7885f2ad7738a7a587a321e4
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-14 00:02:20 +0900

    BackEnd: @postMapping ResponseEntity 형식으로 수정 ResultDTOList 반환 @GetMapping 추가

--- mreivew/src/main/java/com/example/mreivew/controller/UploadController.java
+++ mreivew/src/main/java/com/example/mreivew/controller/UploadController.java
@@ -1,18 +1,28 @@
 package com.example.mreivew.controller;
 
+import com.example.mreivew.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import net.bytebuddy.description.field.FieldList;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -23,13 +33,13 @@ public class UploadController {
 	private String uploadPath;
 
 	@PostMapping("/uploadAjax")
-	public void uploadFile(MultipartFile[] uploadFiles){
-
+	public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+		List<UploadResultDTO> resultDTOList = new ArrayList<>();
 		for(MultipartFile uploadFile : uploadFiles){
 
 			if(uploadFile.getContentType().startsWith("image")==false){
 				log.warn("this file is not image type");
-				return;
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
 			}
 
 			//실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
@@ -50,12 +60,15 @@ public class UploadController {
 			Path savePath = Paths.get(saveName);
 
 			try{
-				uploadFile.transferTo(savePath);
+				uploadFile.transferTo(savePath);//실제 이미지 저장
+				resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
 			} catch(IOException e){
 				e.printStackTrace();
 			}
 
 		}//end for
+
+		return new ResponseEntity<>(resultDTOList,HttpStatus.OK);
 	}
 
 	private String makeFolder(){
@@ -70,5 +83,25 @@ public class UploadController {
 		}
 		return folderPath;
 
+	}
+
+	@GetMapping("/display")
+	public ResponseEntity<byte[]> getFile(String fileName){
+		ResponseEntity<byte[]> result =null;
+		try{
+			String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+			log.info("fileName: "+srcFileName);
+			File file = new File(uploadPath + File.separator+ srcFileName);
+			log.info("file: "+file);
+			HttpHeaders header = new HttpHeaders();
+
+			//MIME타입 처리
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file),header,HttpStatus.OK);
+		} catch (Exception e){
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return result;
 	}
 }
