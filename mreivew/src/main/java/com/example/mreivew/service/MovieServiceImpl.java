bd426fcc54bc0e6fa1f8834fc27166639f2a86f4
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-19 16:29:02 +0900

    Backend :register upload FrontEnd : upload 처리

--- mreivew/src/main/java/com/example/mreivew/service/MovieServiceImpl.java
+++ mreivew/src/main/java/com/example/mreivew/service/MovieServiceImpl.java
@@ -1,10 +1,17 @@
 package com.example.mreivew.service;
 
 import com.example.mreivew.dto.MovieDTO;
+import com.example.mreivew.entity.Movie;
+import com.example.mreivew.entity.MovieImage;
+import com.example.mreivew.repository.MovieImageRepository;
 import com.example.mreivew.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Log4j2
@@ -12,8 +19,23 @@ import org.springframework.stereotype.Service;
 public class MovieServiceImpl implements MovieService{
 	private final MovieRepository movieRepository;
 
+	private final MovieImageRepository imageRepository;
+
+	@Transactional
 	@Override
 	public Long register(MovieDTO movieDTO) {
-		return null;
+		Map<String,Object> entityMap = dtoToEntity(movieDTO);
+		Movie movie =(Movie) entityMap.get("movie");
+		List<MovieImage> movieImageList = (List<MovieImage>) entityMap.get("imgList");
+
+		movieRepository.save(movie);
+
+		movieImageList.forEach(movieImage -> {
+			imageRepository.save(movieImage);
+		});
+
+		return movie.getMno();
 	}
+
+
 }
