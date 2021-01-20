ff7f41f7f38373cb1aafcf910e26a820aa120cd4
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-20 17:59:08 +0900

    BackEnd : ReviewRepository 생성 리뷰 랜덤으로 200개 등록

---
+++ mreview/src/test/java/com/example/mreview/repository/ReviewRepositoryTests.java
@@ -0,0 +1,39 @@
+package com.example.mreview.repository;
+
+import com.example.mreview.entity.Member;
+import com.example.mreview.entity.Movie;
+import com.example.mreview.entity.Review;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReviewRepositoryTests {
+
+	@Autowired
+	private ReviewRepository reviewRepository;
+
+	@Test
+	public void insertMovieReviews(){
+		//200개 리뷰 등록
+		IntStream.rangeClosed(1, 200).forEach(i -> {
+			//영화번호
+			Long mno = (long)(Math.random()*100)+1;
+
+			//리뷰어번호(멤버번호 리뷰 등록자번호임)
+			Long mid = ((long)(Math.random()*100)+1);
+			Member member = Member.builder().
+					mid(mid).build();
+
+			Review movieReview = Review.builder()
+					.member((member))
+					.movie(Movie.builder().mno(mno).build())
+					.grade((int)(Math.random()*5)+1)
+					.text("이 영화에 대한 느낌...."+i)
+					.build();
+			reviewRepository.save(movieReview);
+		});
+	}
+}
