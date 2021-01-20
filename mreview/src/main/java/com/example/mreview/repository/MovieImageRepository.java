af67f9b885b9531f94d87ea085f9a6a897a14142
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-20 18:48:20 +0900

    BackEnd: imagerepository 등록 특정영화 조회, 페이지 처리

--- mreview/src/main/java/com/example/mreview/repository/MovieImageRepository.java
+++ mreview/src/main/java/com/example/mreview/repository/MovieImageRepository.java
@@ -4,4 +4,5 @@ import com.example.mreview.entity.MovieImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieImageRepository extends JpaRepository<MovieImage,Long> {
+
 }
