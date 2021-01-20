81698d2657f7cbeb6d822a4713800419c4d81c91
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-20 16:38:14 +0900

    전부삭제

--- mreivew/src/main/java/com/example/mreivew/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mreivew.repository;
-
-import com.example.mreivew.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-}
--- mreivew/src/main/java/com/example/mreivew/repository/MovieImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mreivew.repository;
-
-import com.example.mreivew.entity.MovieImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieImageRepository extends JpaRepository<MovieImage,Long> {
-}
