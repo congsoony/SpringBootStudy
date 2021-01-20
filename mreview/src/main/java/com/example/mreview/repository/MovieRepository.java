8fdd2f561de11a9a185ba97fe9c62d202eb0aac6
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-20 17:36:56 +0900

    BackEnd : movie , movieimage repository 추가 movie 100개 데이터 추가 image경로 1~5랜덤으로 추가

---
+++ mreview/src/main/java/com/example/mreview/repository/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.example.mreview.repository;
+
+import com.example.mreview.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+
+}
