f5404348e165978a2df6c1ee2fc93b5ae270e751
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-21 15:43:06 +0900

    BackEnd: Movie, MovieImage DTO 생성

---
+++ mreview/src/main/java/com/example/mreview/dto/MovieDTO.java
@@ -0,0 +1,25 @@
+package com.example.mreview.dto;
+
+import com.example.mreview.entity.MovieImage;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieDTO {
+
+	private Long mno;
+
+	private String title;
+
+	@Builder.Default
+	private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+	
+}
