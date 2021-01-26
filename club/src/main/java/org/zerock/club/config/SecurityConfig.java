371aab711e6fa6be3b0bfa80e588a01d635243d7
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-26 15:04:45 +0900

    사용자 user 생성 password 1111 이고 권한을 USER를 준 계정을 만들어 로그인 테스트

--- club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.zerock.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,5 +15,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		//사용자 계정은 user1
+		auth.inMemoryAuthentication().withUser("user1")
+				//1111 패스워드 인코딩 결과
+				.password("$2a$10$EOHILjCWCbUgYmGn8bBv0uG.DEtPpR.ALEDhPS3fGKoMiMW//a5Ay")
+				.roles("USER");
 	}
 }
